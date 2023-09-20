a7e9d59b16990ae06fe1562f4064984bd7ff0ada
Dora Yaroshenko <yar-dora@yandex.ru>
2023-09-20 21:11:10 +0300

    Update app.js

--- app.js
+++ app.js
@@ -29,13 +29,6 @@ app.use(express.json({limit:'5mb'}));
 // להגדיר תיקייה סטטית שתיהיה התיקייה בשם פאבליק
 app.use(express.static(path.join(__dirname,"public")));
 
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  res.setHeader('Access-Control-Allow-Credentials', 'true');
-});
-
 routesInit(app);
 
 
